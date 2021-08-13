510de40d5e170e19a73760e2d14eb187e0712f4a
nanaco541 <vre343434@gmail.com>
2021-08-13 12:55:18 +0800

    update

---
+++ Assets/Scripts/DefenderButton.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderButton : MonoBehaviour
+{
+
+    private void OnMouseDown()
+    {
+        var buttons = FindObjectsOfType<DefenderButton>();
+        foreach(DefenderButton button in buttons)
+        {
+            button.GetComponent<SpriteRenderer>().color = new Color32(72, 72, 72, 255);
+        }
+
+        GetComponent<SpriteRenderer>().color = Color.white;
+
+    }
+
+}
